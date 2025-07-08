9581a375ea6b5dc795112794c57eca744b632ea2
SRIJITH11101 <dovetonsrijith29@gmail.com>
2025-07-09 02:30:04 +0530

    intial config

---
+++ src/test/java/com/moviesprime/cineapi/CineapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesprime.cineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
